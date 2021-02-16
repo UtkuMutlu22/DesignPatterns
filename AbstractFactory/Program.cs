e071a1baed94eb784f41fc0adbf7f829031c1c9c
utku mutlu <utku.mutlu22@outlook.com.tr>
2021-02-16 22:31:45 +0300

    Add project files.

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new Factory1());
+            productManager.GetAll();
+        }
+    }
+    public abstract class Logging
+    {
+        public abstract void Log(string message);
+        
+    }
+    public class Log4NetLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with Log4Netlogger");
+        }
+    }
+    public class NLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with Nlogger");
+        }
+    }
+    public abstract class Caching
+    {
+        public abstract void Cache(string data);
+    }
+    public class MemmCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("MemCache");
+        }
+    }
+    public abstract class CrossCuttingConcernsFactory1
+    {
+        public abstract Logging CreateLogger();
+        public abstract Caching CreateCaching();
+
+    }
+    public class Factory1 : CrossCuttingConcernsFactory1
+    {
+        public override Caching CreateCaching()
+        {
+            return new MemmCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new NLogger();
+        }
+    }
+    public class ProductManager
+    {
+       private CrossCuttingConcernsFactory1 _crossCuttingConcernsFactory1;
+       private Logging _logging;
+        private Caching _caching;
+        public ProductManager(CrossCuttingConcernsFactory1 crossCuttingConcernsFactory1)
+        {
+            _crossCuttingConcernsFactory1 = crossCuttingConcernsFactory1;
+            _logging = _crossCuttingConcernsFactory1.CreateLogger();
+            _caching = _crossCuttingConcernsFactory1.CreateCaching();
+        }
+
+        public void GetAll()
+        {
+            _logging.Log("logged");
+            _caching.Cache("Data");
+
+            Console.WriteLine("product Listed");
+        }
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory2());
+            customerManager.Save();
+        }
+    }
+    public class LoggerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new EdLogger();
+        }
+    }
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new Log4NetLogger();
+        }
+    }
+
+    internal class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Loggin log4net");
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+    public class EdLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("logged");
+        }
+    }
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
